f5616b84717aecd13ca77853391a115a273f217e
gerson05 <1115449382@u.icesi.edu.co>
2023-04-29 14:14:37 -0500

    method increase quantity product

--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -5,60 +5,81 @@ import java.util.ArrayList;
 
 import exceptions.NotNumberNegative;
 
-public class Inventory{
+public class Inventory {
     private ArrayList<Order> orders;
     private ArrayList<Product> products;
-    
-    public Inventory(){
+
+    public Inventory() {
         orders = new ArrayList<>();
         products = new ArrayList<>();
     }
 
-    public void addProduct(String name, String desc, double price, int quantity, int category){
+    public void addProduct(String name, String desc, double price, int quantity, int category) {
         boolean search;
-        if(quantity<0||price<0.0){
+        if (quantity < 0 || price < 0.0) {
             throw new NotNumberNegative();
-        }else{
+        } else {
             search = searchExistenProduct(name, desc);
-            if(search==false){
-                Product nProduct= new Product(name, desc, price, quantity);
+            if (search == false) {
+                Product nProduct = new Product(name, desc, price, quantity);
                 this.products.add(nProduct);
                 nProduct.chooseCategory(category);
-            }else{
+            } else {
                 System.out.println("Existent product");
             }
         }
     }
-    
-    private boolean searchExistenProduct(String name, String desc){
-        boolean exist= false;
-        for(Product p:products){
-            if(p.getName().equalsIgnoreCase(name) && p.getDesc().equalsIgnoreCase(desc)){
-                exist=true;
+
+    private boolean searchExistenProduct(String name, String desc) {
+        boolean exist = false;
+        for (Product p : products) {
+            if (p.getName().equalsIgnoreCase(name) && p.getDesc().equalsIgnoreCase(desc)) {
+                exist = true;
             }
         }
         return exist;
     }
-    public void addOrder(String bName, ArrayList<Product> list, String date) throws ParseException{
-        if(!list.isEmpty()){
+
+    public void addOrder(String bName, ArrayList<Product> list, String date) throws ParseException {
+        if (!list.isEmpty()) {
             double totalPrice = calcTotal(list);
             orders.add(new Order(bName, list, totalPrice, date));
             lessQuantityMorePurch(list);
         }
     }
 
-    private void lessQuantityMorePurch(ArrayList<Product> list){
-        for(int i=0;i<list.size();i++){
-            list.get(i).setQuantity(list.get(i).getQuantity()-1);
-            list.get(i).setPurchased(list.get(i).getPurchased()+1);
+    private void lessQuantityMorePurch(ArrayList<Product> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).setQuantity(list.get(i).getQuantity() - 1);
+            list.get(i).setPurchased(list.get(i).getPurchased() + 1);
         }
     }
-    private double calcTotal(ArrayList<Product> list){
-        double result=0.0;
-        for(Product p: list){
-            result+=p.getPrice();
+
+    private double calcTotal(ArrayList<Product> list) {
+        double result = 0.0;
+        for (Product p : list) {
+            result += p.getPrice();
         }
         return result;
+    }
+
+    private String increaseQuantityProduct(String name, int cant) {
+        Product current = null;
+        current = searchProduct(name);
+        if (current != null) {
+            int quantity= current.getQuantity()+cant;
+            current.setQuantity(quantity);
+        }
+        return "the new quantity for product: "+ name +" is: "+ current.getQuantity();
+    }
+
+    public Product searchProduct(String name) {
+        for (Product p : products) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
+        return null;
     }
 
     public ArrayList<Order> getOrders() {
