cfdc92ac02f9c5da1bc1b353d25406e90ab569a5
gerson05 <1115449382@u.icesi.edu.co>
2023-04-29 14:48:57 -0500

    increase quantity product and search product for price

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,4 +1,5 @@
 package model;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ public class Order {
     private ArrayList<Product> list;
     private double totalPrice;
     private Date purchasedDate;
-    
+
     public Order(String bName, ArrayList<Product> list, double totalPrice, String date) throws ParseException {
         this.bName = bName;
         this.list = list;
@@ -17,30 +18,37 @@ public class Order {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         purchasedDate = dateFormat.parse(date);
     }
+
     public String getbName() {
         return bName;
     }
+
     public void setbName(String bName) {
         this.bName = bName;
     }
+
     public ArrayList<Product> getList() {
         return list;
     }
+
     public void setList(ArrayList<Product> list) {
         this.list = list;
     }
+
     public double getTotalPrice() {
         return totalPrice;
     }
+
     public void setTotalPrice(double totalPrice) {
         this.totalPrice = totalPrice;
     }
+
     public Date getPurchasedDate() {
         return purchasedDate;
     }
+
     public void setPurchasedDate(Date purchasedDate) {
         this.purchasedDate = purchasedDate;
     }
-    
 
 }
